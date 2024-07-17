d16a22628f883486cc096d6d1f9798d67bcdbde8
as08angelo <angelostimoli08@gmail.com>
2024-07-17 08:48:29 +0200

    Code1707

---
+++ shoponline/src/components/GridCard.tsx
@@ -0,0 +1,15 @@
+import mockProducts from "../interfaces/productInterface";
+import ProductCard from "./ProductCard";
+import '../css/GridCard.css';
+
+export default function GridCard(){
+    return (
+        <div className="center-grid">
+            <div className="products-grid">
+                {mockProducts.map ((product) => (
+                    <ProductCard key={product.id} product={product}/>
+                ))}
+            </div>
+        </div>
+        );        
+}
